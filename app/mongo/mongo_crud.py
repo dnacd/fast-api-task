baf34dea2083cd68e2ece0b6cf607bca6da94721
D. H <danoacd@gmail.com>
2021-12-15 12:48:47 +0100

    Update pagination logic

--- app/mongo/mongo_crud.py
+++ app/mongo/mongo_crud.py
@@ -1,8 +1,9 @@
+import asyncio
+
 from pymongo.collection import Collection
 from fastapi.encoders import jsonable_encoder
 from bson.objectid import ObjectId
 from functools import lru_cache
-from math import ceil
 
 from config import Settings
 from mongo.db_mongo import get_mongo_db
@@ -34,24 +35,13 @@ class ContentCRUD:
         created_post = await self.get_collection().find_one({"_id": data.inserted_id})
         return created_post
 
-    async def get_post_list(self, page_size, page_num, logged=False):
-        unlogged = await self.get_collection().find({'logged_only': False}).to_list(10000)
-        if (page_size and page_num) is not None:
-            skips = page_size * (page_num - 1)
-            data = await self.get_collection().find().skip(skips).limit(page_size).to_list(10000) if logged \
-                else unlogged
-            count = await self.get_collection().count_documents({}) if logged \
-                else await self.get_collection().count_documents({'logged_only': False})
-            items = {
-                "items": data,
-                "page_size": page_size,
-                "page_num": page_num,
-                "total_pages": ceil(count / page_size),
-                "total_docs": count
-            }
-            return items
+    async def get_post_list(self, logged=False, paginate=False, page_size=None, skips=None):
+        queryset = self.get_collection().find() if logged else self.get_collection().find({'logged_only': False})
+        if paginate:
+            queryset = await queryset.skip(skips).limit(page_size).to_list(10000)
         else:
-            return await self.get_collection().find().to_list(10000) if logged else unlogged
+            queryset = await queryset.to_list(1000)
+        return queryset
 
     async def update_post(self, post_id: str, update_data: dict):
         result = await self.get_collection().update_one({'_id': ObjectId(post_id)}, {'$set': update_data})
--- app/routers/mongo.py
+++ app/routers/mongo.py
@@ -1,10 +1,11 @@
+from math import ceil
 from typing import Optional
 from fastapi import APIRouter, Body, Depends
 from security.auth import AuthUser
 from security.header import api_key_header
 
 from mongo.mongo_crud import content_crud
-from schemas import PostCreateSchemaMongo
+from schemas import PostCreateSchemaMongo, PostViewSchemaMongo
 
 router = APIRouter(
     prefix="/mongo",
@@ -14,20 +15,36 @@ router = APIRouter(
 
 
 @router.post("/new_post", response_description="Create new Post", response_model=PostCreateSchemaMongo)
-async def create_post(post: PostCreateSchemaMongo = Body(...)):
+async def create_post(post: PostCreateSchemaMongo):
     await content_crud.new_post(post)
     return PostCreateSchemaMongo.dict(post)
 
 
-@router.get("/post_list", response_description="List of posts",
-            dependencies=(api_key_header,))
+@router.get("/post_list",
+            response_description="List of posts",
+            dependencies=(api_key_header,),
+            response_model=PostViewSchemaMongo)
 async def post_list_mongo(page: Optional[int] = None,
                           size: Optional[int] = None,
                           authorize: AuthUser = Depends()):
     await authorize.requires_access_token(required=False)
     logged = True if authorize.get_user_or_none() is not None else None
-    data = await content_crud.get_post_list(page_num=page, page_size=size, logged=logged)
-    return data
+    if (page and size) is not None:
+        skips = size * (page - 1)
+        count = await content_crud.get_collection().count_documents({}) if logged \
+            else await content_crud.get_collection().count_documents({'logged_only': False})
+        data = await content_crud.get_post_list(logged=logged, paginate=True, page_size=size, skips=skips)
+        items = {
+            "items": data,
+            "page_size": size,
+            "page_num": page,
+            "total_pages": ceil(count / size),
+            "total_docs": count
+        }
+        return items
+    else:
+        data = await content_crud.get_post_list(logged=logged)
+        return data
 
 
 @router.get("/post/{post_id}", response_description="Post Detail", response_model=PostCreateSchemaMongo)
