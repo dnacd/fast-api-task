068e01e25b73fd18f871e21e20cf75768134617b
D. H <danoacd@gmail.com>
2021-12-17 11:17:25 +0100

    add merge helper with  merge_user_data to remove duplicate

--- app/mongo/pg_mongo_aggregation.py
+++ app/mongo/pg_mongo_aggregation.py
@@ -2,18 +2,19 @@ from models import User
 from schemas.mongo.post_schemas import ViewListUserSchema
 
 
+def merge_helper(data, users_dict):
+    data.author = users_dict.get(data.author_id)
+    for comment in data.comments:
+        if users_dict.get(comment.user_id):
+            comment.username = users_dict.get(comment.user_id).username
+
+
 async def merge_user_data(data, single=False):
     users = [ViewListUserSchema.from_orm(user) for user in await User.all()]
     users_dict = {user.id: user for user in users}
     if not single:
         for post in data:
-            post.author = users_dict.get(post.author_id)
-            for comment in post.comments:
-                if users_dict.get(comment.user_id):
-                    comment.username = users_dict.get(comment.user_id).username
+            merge_helper(post, users_dict)
     else:
-        data.author = users_dict.get(data.author_id)
-        for comment in data.comments:
-            if users_dict.get(comment.user_id):
-                comment.username = users_dict.get(comment.user_id).username
+        merge_helper(data, users_dict)
     return data
